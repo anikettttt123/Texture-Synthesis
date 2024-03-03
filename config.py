08039372059a9b82b4baa028655d86d1f0ddf45b
Aniket Pratap Singh <94516890+anikettttt123@users.noreply.github.com>
2024-03-03 14:36:53 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,75 @@
+import os
+from tools import create_dir
+from data_io import get_texture_iter
+
+create_dir('samples')               # create, if necessary, for the output samples 
+create_dir('models') 
+home        = os.path.expanduser("~")
+
+
+def zx_to_npx(zx, depth):
+    '''
+    calculates the size of the output image given a stack of 'same' padded
+    convolutional layers with size depth, and the size of the input field zx
+    '''
+    # note: in theano we'd have zx*2**depth
+    return (zx - 1)*2**depth + 1
+
+
+class Config(object):
+    '''
+    wraps all configuration parameters in 'static' variables -- these are not serialized!!
+    '''
+    
+    ##optimization constants
+    lr          = 0.0002                # learning rate of adam
+    b1          = 0.5                   # momentum term of adam
+    l2_fac      = 1e-8                  # L2 weight regularization factor
+    epoch_count = 100                    #how many epochs to do globally    
+    k           = 1                     # number of D updates vs G updates
+    batch_size  = 25
+    epoch_iters =batch_size * 1000      #steps inside one epoch 
+                 
+    ##constructor to define serializable variables, serialized when dumping model
+    def __init__(self):    
+        ##
+        # sampling parameters    
+        self.nz_local = 30    
+        self.nz_global = 60                 # num of global Z dimensions
+        self.nz_periodic = 3                    # num of global Z dimensions
+        self.nz_periodic_MLPnodes = 50          # the MLP gate for the neural network
+        self.nz          = self.nz_local+self.nz_global+self.nz_periodic*2                   # num of dim for Z at each field position, sum of local, global, periodic dimensions
+        self.periodic_affine = False            # if True planar waves sum x,y sinusoids, else axes aligned sinusoids x or y 
+        self.zx          = 6                    # number of spatial dimensions in Z
+        self.zx_sample   = 32                   # size of the spatial dimension in Z for producing the samples    
+        self.zx_sample_quilt = self.zx_sample/4      # how many tiles in the global dimension quilt for output sampling
+
+        ##
+        # network parameters
+        self.nc          = 3                     # number of channels in input X (i.e. r,g,b)
+        self.gen_ks      = ([(5,5)] * 5)[::-1]   # kernel sizes on each layer - should be odd numbers for zero-padding stuff
+        self.dis_ks      = [(5,5)] * 5           # kernel sizes on each layer - should be odd numbers for zero-padding stuff
+        self.gen_ls      = len(self.gen_ks)           # num of layers in the generative network
+        self.dis_ls      = len(self.dis_ks)           # num of layers in the discriminative network
+        self.gen_fn      = [self.nc]+[2**(n+6) for n in range(self.gen_ls-1)]  # generative number of filters
+        self.gen_fn      = self.gen_fn[::-1]
+        self.dis_fn      = [2**(n+6) for n in range(self.dis_ls-1)]+[1]   # discriminative number of filters        
+        self.npx         = zx_to_npx(self.zx, self.gen_ls) # num of pixels width/height of images in X
+        
+        ##input texture folder
+        self.sub_name    = "honey"#'hex1'#
+        self.texture_dir = home + "/DILOG/dcgan_code-master/texture_gan/%s/" % self.sub_name
+        self.save_name   = self.sub_name+ "_filters%d_npx%d_%dgL_%ddL_%dGlobal_%dPeriodic_%sAffine_%dLocal" % (self.dis_fn[0],self.npx,self.gen_ls, self.dis_ls,self.nz_global,self.nz_periodic,self.periodic_affine ,self.nz_local)
+        self.load_name   = None                  # if None, initializing network from scratch
+           
+    ## gives back the correct data iterator given class variables -- this way we avoid the python restriction not to pickle iterator objects
+    def data_iter(self):
+        return get_texture_iter(self.texture_dir, npx=self.npx, mirror=False, batch_size=self.batch_size)
+
+    def print_info(self):
+        ##
+        # output some information
+        print ("Learning and generating samples from zx ", self.zx, ", which yields images of size npx ", zx_to_npx(self.zx, self.gen_ls))
+        print ("Producing samples from zx_sample ", self.zx_sample, ", which yields images of size npx ", zx_to_npx(self.zx_sample, self.gen_ls) )
+        print ("Saving samples and model data to file ",self.save_name)
+
---
+++ demo_generation.py
@@ -0,0 +1,72 @@
+from psgan import PSGAN,sample_noise_tensor
+import sys
+import numpy as np
+from data_io import save_tensor
+
+if len(sys.argv) <=1:
+    print ("please give model filename")
+    print ("e.g. checked github model hex1_filters64_npx161_5gL_5dL_0Global_3Periodic_15Local_epoch43.sgan")
+    raise Exception('no filename specified')
+
+name = sys.argv[1]
+print ("using stored model",name)
+
+##sample a periodically tiling texture
+def mosaic_tile(psgan,NZ1=12,NZ2=12, repeat=(2,3)):
+    ovp = 2  # how many z values should we keep for overlap, for 5 layer architecture and (5,5) kernels 2 is enough
+    tot_subsample= 2**psgan.gen_depth   
+    print ("NZ1 NZ2 for tilable texture: ", NZ1, NZ2)
+
+    sample_zmb = sample_noise_tensor(psgan.config,1,max(NZ1,NZ2))[:,:,:NZ1,:NZ2] 
+
+
+    sample_zmb[:, :, :, -ovp * 2:] = sample_zmb[:, :, :, :ovp * 2]
+    sample_zmb[:, :, -ovp * 2:, :] = sample_zmb[:, :, :ovp * 2, :]
+    samples = psgan.generate(sample_zmb)
+    
+    #measure the optimal offset of pixels we crop from the edge of the tile: this should have loss of 0 if the tile is perfectly periodical
+    ##note: for Theano code we had a nice analytical formula for the optimal offset
+    ##note: for the Lasagna code we calculate this empirically since the conv. arithmetic is not well documented and varies depending on NZ1 and NZ2
+    ## calc. the pixel discrepancy btw the left and right column, and top and bottom row, when cropping crop1 and crop2 pixels
+    def offsetLoss(crop1,crop2):
+        return np.abs(samples[:, :, :, crop1] - samples[:, :, :, -crop2]).mean()+ np.abs(samples[:, :, crop1] - samples[:, :, -crop2]).mean()    
+    best=1e6
+    crop1=0
+    crop2=0
+    for i in range(ovp*tot_subsample/2,ovp*tot_subsample):
+        for j in range(ovp*tot_subsample/2,ovp*tot_subsample):
+            loss = offsetLoss(i,j)
+            if loss < best:
+                best=loss
+                crop1=i
+                crop2=j
+
+    print ("optimal offsets",crop1,crop2,"offset edge errors",best)   
+    samples = samples[:, :, crop1:-crop2, crop1:-crop2]
+    s = (samples.shape[2],samples.shape[3])   
+    print ("tile sample size", samples.shape)
+    save_tensor(samples[0],"samples/TILE_%s_%s.jpg" % (name.replace('/','_'), s))
+ 
+    if repeat is not None:
+        sbig = np.zeros((3,repeat[0] * s[0], repeat[1] * s[1]))
+        for i in range(repeat[0]):
+            for j in range(repeat[1]):
+                sbig[:,i * s[0]:(i + 1) * s[0], j * s[1]:(j + 1) * s[1]] = samples[0]
+        save_tensor(sbig,"samples/TILE_%s_%s_%s.jpg" % (name.replace('/','_'), s, repeat))
+    return
+
+#sample a random texture
+def sample_texture(psgan,NZ1=60,quilt_tile = 20):    
+    z_sample        = sample_noise_tensor(psgan.config,1,NZ1,quilt_tile) 
+    data = psgan.generate(z_sample)
+    save_tensor(data[0], 'samples/stored_%s.jpg' % (name.replace('/','_')))
+
+psgan        = PSGAN(name=name)
+c=psgan.config
+print ("nz",c.nz, "global Dimensions",c.nz_global,"periodic Dimensions",c.nz_periodic)
+print ("G values",c.gen_fn,c.gen_ks)
+print ("D values",c.dis_fn, c.dis_ks)
+
+sample_texture(psgan)
+mosaic_tile(psgan)
+
